3a056da0ea301417a5c588bc74e4cb7d049b136c
Marik28 <markel20002000@gmail.com>
2021-08-08 18:16:26 +0600

    first commit

---
+++ models.py
@@ -0,0 +1,87 @@
+import datetime
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class AttachmentType(Enum):
+    PHOTO = "photo"
+    POSTED_PHOTO = "posted_photo"
+    VIDEO = "video"
+    AUDIO = "audio"
+    DOC = "doc"
+    GRAFFITI = "graffiti"
+    LINK = "link"
+    NOTE = "note"
+    APP = "app"
+    POLL = "poll"
+    PAGE = "page"
+    ALBUM = "album"
+    PHOTOS_LIST = "photos_list"
+    MARKET = "market"
+    MARKET_ALBUM = "market_album"
+    STICKER = "sticker"
+    PRETTY_CARDS = "pretty_cards"
+    EVENT = "event"
+
+
+class SizeType(Enum):
+    S = "s"
+    M = "m"
+    X = "x"
+    O = "o"
+    P = "p"
+    Q = "q"
+    R = "r"
+    Y = "y"
+    Z = "z"
+    W = "W"
+
+
+class PhotoSize(BaseModel):
+    type: SizeType
+    url: str
+    width: int
+    height: int
+
+
+class Photo(BaseModel):
+    id: int
+    album_id: int
+    owner_id: int
+    user_id: int
+    text: str
+    date: datetime.datetime
+    sizes: list[PhotoSize]
+    width: Optional[int]
+    height: Optional[int]
+
+
+class Video(BaseModel):
+    id: int
+    owner_id: int
+    title: str
+    description: str
+    duration: int
+    image: ...
+    first_frame: ...
+    date: datetime.datetime
+    views: int
+    width: int
+    height: int
+
+
+class Attachment(BaseModel):
+    type: AttachmentType
+    photo: Optional[Photo]
+    video: Optional[Video]
+
+
+class WallPost(BaseModel):
+    id: int
+    owner_id: int
+    from_id: int
+    date: datetime.datetime
+    text: str
+    attachments: list[Attachment]
